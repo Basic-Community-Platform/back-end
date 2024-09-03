573ac1973a3a7f654e04ff65434e57f7b3ff8995
한재경 <hjg000223@gmail.com>
2024-09-03 15:18:10 +0900

    feat: 날씨 자동 업데이트 위한 @EnableJpaAuditing 추가

--- src/main/java/opensocial/org/community_hub/CommunityHubApplication.java
+++ src/main/java/opensocial/org/community_hub/CommunityHubApplication.java
@@ -2,12 +2,13 @@ package opensocial.org.community_hub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing //날짜 자동 업데이트 위해
 public class CommunityHubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityHubApplication.class, args);
 	}
-
 }
