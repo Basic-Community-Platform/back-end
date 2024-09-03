2a564cf07afdcb68f133f683ad5d5111ef71d0cb
한재경 <hjg000223@gmail.com>
2024-09-03 18:27:13 +0900

    feat: Post Entity @EntityListeners 추가
    날짜 자동 업데이트

--- src/main/java/opensocial/org/community_hub/entity/Post.java
+++ src/main/java/opensocial/org/community_hub/entity/Post.java
@@ -4,12 +4,14 @@ import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Data
 @Table(name = "posts")
+@EntityListeners(AuditingEntityListener.class)
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
