9219cba0c1e2937c61b70aaf8e01077e56972643
한재경 <hjg000223@gmail.com>
2024-09-03 18:25:17 +0900

    feat: SecurityConfig 요청 인증 범위 수정(전체로, 추후 수정 필요)

--- src/main/java/opensocial/org/community_hub/config/SecurityConfig.java
+++ src/main/java/opensocial/org/community_hub/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/login", "/api/users/**").permitAll() // 모든 사용자에게 "/" 및 "/login", "/api/users/**" 경로에 접근 허용
+                        .requestMatchers("**").permitAll() // 모든 사용자에게 모든 경로 접근 허용, 추후 특정 경로로 고쳐주기
                         .anyRequest().authenticated() // 나머지 요청은 인증 필요
                 )
                 .csrf(AbstractHttpConfigurer::disable); // CSRF 보호 비활성화
