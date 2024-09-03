ef24c1aaae7b660c12a9b2f495ef8d7a8f777426
한재경 <hjg000223@gmail.com>
2024-09-03 11:52:48 +0900

    feat: 테스트 컨트롤러, 스웨거 config 파일 생성

---
+++ src/main/java/opensocial/org/community_hub/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package opensocial.org.community_hub;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .version("v1.0") //버전
+                .title("CommunityHub API") //이름
+                .description("커뮤니티 플랫폼 프로젝트 API"); //설명
+        return new OpenAPI()
+                .info(info);
+    }
+
+}
