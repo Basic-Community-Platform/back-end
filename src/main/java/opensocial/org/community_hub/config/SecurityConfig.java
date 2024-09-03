b4053be18bdc9150c6ca5fa4a0fa34dff35c8db0
한재경 <hjg000223@gmail.com>
2024-09-03 17:07:51 +0900

    feat: SecurityFilterChain 설정
    모든 사용자에게 현재 모든 경로에 대한 요청 권한 부여, CSRF 보호 비활성화

--- src/main/java/opensocial/org/community_hub/config/SecurityConfig.java
+++ src/main/java/opensocial/org/community_hub/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package opensocial.org.community_hub.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +14,17 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/login", "/api/users/**").permitAll() // 모든 사용자에게 "/" 및 "/login", "/api/users/**" 경로에 접근 허용
+                        .anyRequest().authenticated() // 나머지 요청은 인증 필요
+                )
+                .csrf(AbstractHttpConfigurer::disable); // CSRF 보호 비활성화
+
+        return http.build();
     }
 }
