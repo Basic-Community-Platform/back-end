02710357ae353b3781a837b26c3b9a2b9a4e59a1
한재경 <hjg000223@gmail.com>
2024-09-03 17:18:27 +0900

    feat: User Entity에 날짜자동업데이트 @EntityListeners 추가
    createdAt, updatedAt 제대로 동작하기 위해서는
    1. config에 @EnableJpaAuditing 추가
    2. Entity에 @EntityListeners(AuditingEntityListener.class) 추가

--- src/main/java/opensocial/org/community_hub/entity/User.java
+++ src/main/java/opensocial/org/community_hub/entity/User.java
@@ -4,12 +4,14 @@ import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Data
 @Table(name = "app_user") // 테이블 이름 변경 : sql 예약어(user) 중복 방지
+@EntityListeners(AuditingEntityListener.class) //날짜 자동 업데이트를 위한 애노테이션
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
