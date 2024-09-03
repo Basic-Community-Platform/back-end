df86c3e30ff97a0d4527346370c3d2c8823779fa
한재경 <hjg000223@gmail.com>
2024-09-03 11:51:51 +0900

    feat: 부트 프로젝트 생성

---
+++ src/main/java/opensocial/org/community_hub/CommunityHubApplication.java
@@ -0,0 +1,13 @@
+package opensocial.org.community_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/opensocial/org/community_hub/CommunityHubApplicationTests.java
@@ -0,0 +1,13 @@
+package opensocial.org.community_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
