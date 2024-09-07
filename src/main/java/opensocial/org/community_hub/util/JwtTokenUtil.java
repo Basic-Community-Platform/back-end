3d495dd9d9c0f6f45badbc83793e8e1d0d1c54d6
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-09-07 22:18:05 +0900

    style: JwtTokenUtil에서 Value import 삭제

--- src/main/java/opensocial/org/community_hub/util/JwtTokenUtil.java
+++ src/main/java/opensocial/org/community_hub/util/JwtTokenUtil.java
@@ -5,7 +5,6 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import opensocial.org.community_hub.config.JwtConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
