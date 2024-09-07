23f3f61a7e8c78feb0a1f7af317a9bcecf6ca7de
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-09-07 22:49:15 +0900

    style: SecurityConfig에서 Swagger 엔드포인트 인증 없이 접근 허용

--- src/main/java/opensocial/org/community_hub/config/SecurityConfig.java
+++ src/main/java/opensocial/org/community_hub/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
 
                 //디버깅용 스웨거 경로 허용. 추후 제거하기!
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users/login", "/api/users/register", "/api/users/refresh", "/swagger-ui/index.html").permitAll()  // 로그인과 회원가입, 리프레쉬 발급은 모두 허용
+                        .requestMatchers("/api/users/login", "/api/users/register", "/api/users/refresh", "/swagger-ui/**", "/v3/api-docs/**").permitAll()  // 로그인과 회원가입, 리프레쉬 발급은 모두 허용
                         .anyRequest().authenticated()  // 그 외 모든 요청은 인증 필요
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))  // 세션을 사용하지 않음 (JWT 사용)
