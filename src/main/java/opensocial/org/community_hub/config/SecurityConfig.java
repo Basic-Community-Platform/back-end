a296f4a3e26dc792201c70212291b82d3aec7783
이민선 <woooooooow22@gmail.com>
2024-09-11 12:01:18 +0900

    feat: allowed origins에 배포 URL 추가

--- src/main/java/opensocial/org/community_hub/config/SecurityConfig.java
+++ src/main/java/opensocial/org/community_hub/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -55,10 +56,9 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);  // 자격 증명 허용
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));  // 도메인 설정
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://board-project-two.vercel.app"));
         config.setAllowedMethods(Collections.singletonList("*"));  // 모든 HTTP 메서드 허용
         config.setAllowedHeaders(Collections.singletonList("*"));  // 모든 헤더 허용
-
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);  // 모든 경로에 대해 CORS 설정 적용
         return source;
