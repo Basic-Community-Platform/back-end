18c2a1f3474a05a5378bdb81d9e4aa6c9a2c2fcb
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-09-14 23:30:51 +0900

    feat: PostDTO getter, setter -> @Data로 변경

--- src/main/java/opensocial/org/community_hub/domain/post/dto/PostDTO.java
+++ src/main/java/opensocial/org/community_hub/domain/post/dto/PostDTO.java
@@ -1,5 +1,8 @@
 package opensocial.org.community_hub.domain.post.dto;
 
+import lombok.Data;
+
+@Data
 public class PostDTO {
     private Long postId;
     private String title;
@@ -14,54 +17,6 @@ public class PostDTO {
         this.content = content;
         this.viewCount = viewCount;
         this.commentCount = commentCount;
-        this.userName = userName;
-    }
-
-    public Long getPostId() {
-        return postId;
-    }
-
-    public void setPostId(Long postId) {
-        this.postId = postId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public int getViewCount() {
-        return viewCount;
-    }
-
-    public void setViewCount(int viewCount) {
-        this.viewCount = viewCount;
-    }
-
-    public int getCommentCount() {
-        return commentCount;
-    }
-
-    public void setCommentCount(int commentCount) {
-        this.commentCount = commentCount;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
         this.userName = userName;
     }
 }
