5ee9bdd61cc253e8ba17b691fceadeabc4eca8ae
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-10-05 22:23:09 +0900

    feat: ChatMessageDto type 필드 추가 - CHAT, JOIN, LEAVE enum타입

--- src/main/java/opensocial/org/community_hub/domain/chat/dto/ChatMessageDto.java
+++ src/main/java/opensocial/org/community_hub/domain/chat/dto/ChatMessageDto.java
@@ -2,17 +2,26 @@ package opensocial.org.community_hub.domain.chat.dto;
 
 public class ChatMessageDto {
 
+    private String type;
     private String sender;
     private String content;
     private String roomId;
 
-    public ChatMessageDto() {
-    }
+    public ChatMessageDto() {}
 
-    public ChatMessageDto(String sender, String content, String roomId) {
+    public ChatMessageDto(String type, String sender, String content, String roomId) {
+        this.type = type;
         this.sender = sender;
         this.content = content;
         this.roomId = roomId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     public String getSender() {
