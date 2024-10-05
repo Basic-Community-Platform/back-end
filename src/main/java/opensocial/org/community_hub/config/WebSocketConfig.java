96b5e74cbac85b80e69f657eeefb70027690602d
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-10-05 22:21:50 +0900

    feat: WebSocketConfig 모든 도메인 허용 (임시) - 추후 조정 필요

--- src/main/java/opensocial/org/community_hub/config/WebSocketConfig.java
+++ src/main/java/opensocial/org/community_hub/config/WebSocketConfig.java
@@ -18,6 +18,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatHandler, "/chat").setAllowedOrigins("*");
+        registry.addHandler(chatHandler, "/chat")
+                .setAllowedOrigins("*"); // 모든 도메인 허용 (개발 환경에서만 사용)
     }
 }
