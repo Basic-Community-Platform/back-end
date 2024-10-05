b40c19846fc8f911640197ff3e96577050151bf0
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-10-06 04:19:00 +0900

    feat: ChatController getMessages() 기능 추가

--- src/main/java/opensocial/org/community_hub/domain/chat/controller/ChatController.java
+++ src/main/java/opensocial/org/community_hub/domain/chat/controller/ChatController.java
@@ -1,10 +1,12 @@
 package opensocial.org.community_hub.domain.chat.controller;
 
 import opensocial.org.community_hub.domain.chat.dto.ChatRoomDto;
+import opensocial.org.community_hub.domain.chat.entity.ChatMessage;
 import opensocial.org.community_hub.domain.chat.service.ChatService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/chat")
@@ -16,10 +18,12 @@ public class ChatController {
         this.chatService = chatService;
     }
 
+    //추후에 요청 DTO 추가해서 변경하기
     @PostMapping("/room")
-    public ChatRoomDto createRoom(@RequestParam String name) {
-        return chatService.createChatRoom(name);
+    public ChatRoomDto createRoom(@RequestBody Map<String, String> request) {
+        return chatService.createChatRoom(request.get("name"));
     }
+
 
     @GetMapping("/rooms")
     public List<ChatRoomDto> getAllRooms() {
@@ -29,5 +33,10 @@ public class ChatController {
     @GetMapping("/room/{roomId}")
     public ChatRoomDto getRoom(@PathVariable String roomId) {
         return chatService.findRoomById(roomId);
+    }
+
+    @GetMapping("/room/{roomId}/messages")
+    public List<ChatMessage> getMessages(@PathVariable String roomId) {
+        return chatService.getMessagesByRoomId(roomId);
     }
 }
