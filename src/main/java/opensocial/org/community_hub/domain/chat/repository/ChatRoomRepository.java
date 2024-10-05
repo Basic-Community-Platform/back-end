13be1d3bf4cbe1b4264e918d37abc6bb9758048b
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-10-05 19:53:44 +0900

    feat: 채팅 기능 Repository 추가

---
+++ src/main/java/opensocial/org/community_hub/domain/chat/repository/ChatRoomRepository.java
@@ -0,0 +1,7 @@
+package opensocial.org.community_hub.domain.chat.repository;
+
+import opensocial.org.community_hub.domain.chat.entity.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, String> {
+}
