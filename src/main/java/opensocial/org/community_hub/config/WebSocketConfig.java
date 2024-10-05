881374e1a3ee3e0169b8fffff1504824a78c0097
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-10-05 19:51:55 +0900

    feat: WebSocketConfig 추가

---
+++ src/main/java/opensocial/org/community_hub/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package opensocial.org.community_hub.config;
+
+import opensocial.org.community_hub.domain.chat.handler.ChatHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final ChatHandler chatHandler;
+
+    public WebSocketConfig(ChatHandler chatHandler) {
+        this.chatHandler = chatHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatHandler, "/chat").setAllowedOrigins("*");
+    }
+}
