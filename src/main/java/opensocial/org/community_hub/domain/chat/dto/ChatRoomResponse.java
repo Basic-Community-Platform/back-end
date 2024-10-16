7ad2fb062fe6fa187e3eb93693ed598c87cb288e
한재경 <hjg000223@gmail.com>
2024-10-16 17:33:20 +0900

    feat: ChatRoomResponse에 roomId 필드 추가

--- src/main/java/opensocial/org/community_hub/domain/chat/dto/ChatRoomResponse.java
+++ src/main/java/opensocial/org/community_hub/domain/chat/dto/ChatRoomResponse.java
@@ -2,13 +2,23 @@ package opensocial.org.community_hub.domain.chat.dto;
 
 public class ChatRoomResponse {
 
+    private Long roomId;
     private String name;
 
     public ChatRoomResponse() {
     }
 
-    public ChatRoomResponse(String name) {
+    public ChatRoomResponse(Long roomId, String name) {
+        this.roomId = roomId;
         this.name = name;
+    }
+
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
     }
 
     public String getName() {
