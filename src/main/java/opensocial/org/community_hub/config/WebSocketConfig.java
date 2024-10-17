39ca6fd4eefce56724fe5bb0f5d8c182314dc948
한재경 <hjg000223@gmail.com>
2024-10-17 14:45:53 +0900

    feat: WebSocketConfig jwtHandshakeInterceptor 인터셉터 추가

--- src/main/java/opensocial/org/community_hub/config/WebSocketConfig.java
+++ src/main/java/opensocial/org/community_hub/config/WebSocketConfig.java
@@ -11,14 +11,17 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
 
     private final ChatHandler chatHandler;
+    private final JwtHandshakeInterceptor jwtHandshakeInterceptor;
 
-    public WebSocketConfig(ChatHandler chatHandler) {
+    public WebSocketConfig(ChatHandler chatHandler, JwtHandshakeInterceptor jwtHandshakeInterceptor) {
         this.chatHandler = chatHandler;
+        this.jwtHandshakeInterceptor = jwtHandshakeInterceptor;
     }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(chatHandler, "/ws/chat")
+                .addInterceptors(jwtHandshakeInterceptor)  // 인터셉터 추가
                 .setAllowedOrigins("*"); // 모든 도메인 허용 (개발 환경에서만 사용)
     }
 }
